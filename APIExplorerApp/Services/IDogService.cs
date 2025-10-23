f9fc54ada8d71b9b088b77fee8be3af50c3510ec
Martinz7z <80601115+Martinz7z@users.noreply.github.com>
2025-10-23 11:46:55 +0100

    Add service interfaces for Country, Dog, and Joke APIs

---
+++ APIExplorerApp/Services/IDogService.cs
@@ -0,0 +1,11 @@
+﻿using APIExplorerApp.Models;
+
+namespace APIExplorerApp.Services
+{
+    public interface IDogService
+    {
+        Task<DogImage> GetRandomDogImageAsync();
+        Task<List<string>> GetDogBreedsAsync();
+        Task<List<DogImage>> GetDogImagesByBreedAsync(string breed, int count = 5);
+    }
+}
